d37a86682d98953dc481cbe2d3f8ecfb92c23c3d
Josef Dolezal <pepik.ml@gmail.com>
2017-07-09 23:22:17 +0200

    Add Carthage compatibility to the library

--- Source/Supporting Files/LiveGQL.h
+++ Source/Supporting Files/LiveGQL.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Florian. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for LiveGQL.
 FOUNDATION_EXPORT double LiveGQLVersionNumber;
