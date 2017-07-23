86179b4959d002f448cafc8e2749ce60428ee6cf
Florian Mari <florian.mari@epitech.eu>
2017-07-23 19:51:27 +0200

    remove useless Pods

--- Example/Pods/Target Support Files/JSONCodable/JSONCodable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JSONCodable : NSObject
-@end
-@implementation PodsDummy_JSONCodable
-@end
--- Example/Pods/Target Support Files/LiveGQL/LiveGQL-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LiveGQL : NSObject
-@end
-@implementation PodsDummy_LiveGQL
-@end
